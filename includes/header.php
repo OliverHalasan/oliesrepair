4e897f4b5dfe24a4a109e25eb8f85aa7254a9cc1
Oliver Halasan <john_oliver30@yahoo.com>
2023-01-25 21:48:09 -0700

    olies repair web page setup

---
+++ includes/header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="Author" content="Oliver Halasan">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="icon" type="image/x-icon" href="https://oliverhalasan.com/olies-repair/img/logo.jpg">
+    <title>Olies Repair</title>
+</head>
+
+<body>
+
+    <style>
+        .contact-info-background {
+            background-color: rgba(48, 55, 99, 255);
+        }
+
+        .mobile_view {
+            display: block;
+        }
+
+        @media only screen and (max-width: 1300px) {
+
+            .mobile_view {
+                display: none;
+            }
+
+        }
+    </style>
+
+    <header>
+        <div class="contact-info-background b-1 p-1 text-light mobile_view  ">
+            <div class="">
+                <ul class="d-flex flex-row list-unstyled justify-content-evenly ">
+                    <li>Phone: <a class="text-decoration-none text-light" href="tel:780-612-9918"> <span>(780) 612-9918</span></a></li>
+                    <li>Address: <a class="text-decoration-none text-light" href="https://goo.gl/maps/FiP4bQHQ8dwFict6A"> <span>4403 61 Avenue - Stall #4 Leduc AB T9E 7B6</span></a></li>
+                    <li>Email: <a class="text-decoration-none text-light" href="mailto:info@oliesrepair.com"><span>info@oliesrepair.com</span> </a></li>
+                    <li><span>Weekdays: 9:00am to 5:30pm | Weekends: Close | Holiday: Close</span> </li>
+                </ul>
+            </div>
+
+            <!-- icon for mobile view -->
+
+
+        </div>
+        <nav class="d-flex flex-wrap align-items-center justify-content-between  py-3 mb-4 border-bottom  navbar-expand-sm">
+            <div class="container-fluid d-flex flex-wrap align-items-center ">
+                <a href="/" class="d-flex justify-content-center align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+                    <img src="https://oliverhalasan.com/olies-repair/img/logo.jpg" class="w-25" alt="logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navbar-collapse nav col-12 col-md-auto mb-2 justify-content-center">
+                    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                        <li><a href="#" class="nav-link px-3 fs-4 link-dark">Services</a></li>
+                        <li><a href="#" class="nav-link px-3 fs-4 link-dark">Book a Services</a></li>
+                        <li><a href="#" class="nav-link px-3 fs-4 link-dark">About Us</a></li>
+                        <li><a href="#" class="nav-link px-3 fs-4 link-dark">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+
+
+        </nav>
+    </header>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include 'includes/header.php'; ?>
+
+<main>
+    <iframe class="col-12" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19086.575745067184!2d-113.60316598947065!3d53.274809463993726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x539ff9c9f608719f%3A0xbecf103df8a23621!2sOlie&#39;s%20Repair%20shop%20and%20car%20sales!5e0!3m2!1sen!2sca!4v1674548408962!5m2!1sen!2sca" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</main>
+
+<?php include 'includes/footer.php'; ?>
