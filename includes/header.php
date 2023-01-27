a57e780aa36961beebbf165d1fef56c66d34cd89
Oliver <77119741+OliverHalasan@users.noreply.github.com>
2023-01-27 16:40:18 -0700

    fixing the responsiveness of the index

--- includes/header.php
+++ includes/header.php
@@ -1,70 +1,147 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="Author" content="Oliver Halasan">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="icon" type="image/x-icon" href="https://oliverhalasan.com/olies-repair/img/logo.jpg">
-    <title>Olies Repair</title>
-</head>
-
-<body>
-
-    <style>
-        .contact-info-background {
-            background-color: rgba(48, 55, 99, 255);
-        }
-
-        .mobile_view {
-            display: block;
-        }
-
-        @media only screen and (max-width: 1300px) {
-
-            .mobile_view {
-                display: none;
-            }
-
-        }
-    </style>
-
-    <header>
-        <div class="contact-info-background b-1 p-1 text-light mobile_view  ">
-            <div class="">
-                <ul class="d-flex flex-row list-unstyled justify-content-evenly ">
-                    <li>Phone: <a class="text-decoration-none text-light" href="tel:780-612-9918"> <span>(780) 612-9918</span></a></li>
-                    <li>Address: <a class="text-decoration-none text-light" href="https://goo.gl/maps/FiP4bQHQ8dwFict6A"> <span>4403 61 Avenue - Stall #4 Leduc AB T9E 7B6</span></a></li>
-                    <li>Email: <a class="text-decoration-none text-light" href="mailto:info@oliesrepair.com"><span>info@oliesrepair.com</span> </a></li>
-                    <li><span>Weekdays: 9:00am to 5:30pm | Weekends: Close | Holiday: Close</span> </li>
-                </ul>
-            </div>
-
-            <!-- icon for mobile view -->
-
-
-        </div>
-        <nav class="d-flex flex-wrap align-items-center justify-content-between  py-3 mb-4 border-bottom  navbar-expand-sm">
-            <div class="container-fluid d-flex flex-wrap align-items-center ">
-                <a href="/" class="d-flex justify-content-center align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-                    <img src="https://oliverhalasan.com/olies-repair/img/logo.jpg" class="w-25" alt="logo">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="navbar-collapse nav col-12 col-md-auto mb-2 justify-content-center">
-                    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                        <li><a href="#" class="nav-link px-3 fs-4 link-dark">Services</a></li>
-                        <li><a href="#" class="nav-link px-3 fs-4 link-dark">Book a Services</a></li>
-                        <li><a href="#" class="nav-link px-3 fs-4 link-dark">About Us</a></li>
-                        <li><a href="#" class="nav-link px-3 fs-4 link-dark">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-
-
-        </nav>
+<!DOCTYPE html>
+
+<html lang="en">
+
+
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="Author" content="Oliver Halasan">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+    <link rel="icon" type="image/x-icon" href="https://oliverhalasan.com/olies-repair/img/logo.jpg">
+
+    <title>Olies Repair</title>
+
+</head>
+
+
+
+<body>
+
+
+
+    <style>
+        .contact-info-background {
+
+            background-color: rgba(48, 55, 99, 255);
+
+        }
+
+
+
+        .mobile_view {
+
+            display: block;
+
+        }
+
+
+
+        @media only screen and (max-width: 1300px) {
+
+
+
+            .mobile_view {
+
+                display: none;
+
+            }
+
+
+
+        }
+    </style>
+
+
+
+    <header>
+
+        <div class="contact-info-background b-1 p-1 text-light mobile_view  ">
+
+            <div class="">
+
+                <ul class="d-flex flex-row list-unstyled justify-content-evenly ">
+
+                    <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+                        </svg><a class="text-decoration-none text-light ms-2" href="tel:780-612-9918"> <span>(780) 612-9918</span></a></li>
+
+                    <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465-1.281 0-2.462-.172-3.34-.465-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z" />
+                        </svg><a class="text-decoration-none text-light ms-2" href="https://goo.gl/maps/FiP4bQHQ8dwFict6A"> <span>4403 61 Avenue - Stall #4 Leduc AB T9E 7B6</span></a></li>
+
+                    <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-at-fill" viewBox="0 0 16 16">
+  <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2H2Zm-2 9.8V4.698l5.803 3.546L0 11.801Zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 9.671V4.697l-5.803 3.546.338.208A4.482 4.482 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671Z"/>
+  <path d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034v.21Zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791Z"/>
+</svg><a class="text-decoration-none text-light ms-2" href="mailto:info@oliesrepair.com"><span>info@oliesrepair.com</span> </a></li>
+
+                    <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-event" viewBox="0 0 16 16">
+  <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
+  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
+</svg><span class="ms-2">Weekdays: 9:00am to 5:30pm | Weekends: Close | Holiday: Close</span> </li>
+
+                </ul>
+
+            </div>
+
+
+
+            <!-- icon for mobile view -->
+
+
+
+
+
+        </div>
+
+        <nav class="d-flex flex-wrap align-items-center justify-content-between  py-3 mb-4 border-bottom  navbar-expand-sm">
+
+            <div class="container-fluid d-flex flex-wrap align-items-center ">
+
+                <a href="/" class="d-flex justify-content-center align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+
+                    <img src="https://oliverhalasan.com/olies-repair/img/logo.jpg" class="w-25" alt="logo">
+
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+
+                    <span class="navbar-toggler-icon"></span>
+
+                </button>
+
+                <div class="navbar-collapse nav col-12 col-md-auto mb-2 justify-content-center">
+
+                    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+
+                        <li><a href="#" class="nav-link px-3 fs-4 link-dark">Services</a></li>
+
+                        <li><a href="#" class="nav-link px-3 fs-4 link-dark">Book a Services</a></li>
+
+                        <li><a href="#" class="nav-link px-3 fs-4 link-dark">About Us</a></li>
+
+                        <li><a href="#" class="nav-link px-3 fs-4 link-dark">Contact</a></li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+
+
+
+
+        </nav>
+
     </header>
--- index.php
+++ index.php
@@ -9,13 +9,18 @@
 </style>
 
 <main class="container-fluid">
-    <img src="img/auto-mechanic-working-garage-repair-service.jpg" alt="">
-    <section style="height: 100%;" class="px-4 py-5 my-5 text-center">
-        <div class="d-flex flex-column align-items-center justify-content-center">
+
+    <section style="height: 100%;" class="container-fluid">
+    <div class="px-4 py-5 my-5 text-center d-flex flex-row align-items-center">
+         <div class="d-flex flex-column align-items-center justify-content-center col-5">
             <h1 class="">Auto Repair Sercives</h1>
             <p>Olie's Repair is a full auto care for you vehcile. We have invested in all the latest specialist tools and diagnostic software that is specifically tailored for the software in your vehicle. Our auto repair shop is capable of servicing a variety of models. We only do the work that is needed to fix your problem. If you want the quality you would expect from the dealership, but with a more personal and friendly atmosphere, you have found it. </p>
         </div>
-
+        <div class="col-5">
+    <img src="img/auto-mechanic-working-garage-repair-service.jpg" class="img-fluid rounded " alt="">
+    </div>
+    </div>
+       
     </section>
 
     <iframe class="col-12" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19086.575745067184!2d-113.60316598947065!3d53.274809463993726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x539ff9c9f608719f%3A0xbecf103df8a23621!2sOlie&#39;s%20Repair%20shop%20and%20car%20sales!5e0!3m2!1sen!2sca!4v1674548408962!5m2!1sen!2sca" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
